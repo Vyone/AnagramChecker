f44953f45429b1a408b254d0ec9fc14e39da95e2
VyoneL <vyonelouiss@gmail.com>
2025-11-25 03:19:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function clean(word) {
+    return word
+        .toLowerCase()
+        .replace(/\s/g, '')
+        .split('')
+        .sort()
+        .join('');
+}
+
+function areAnagrams(word1, word2) {
+    const cleanedWord1 = clean(word1);
+    const cleanedWord2 = clean(word2);
+    const result = document.getElementById("result");
+
+    if (cleanedWord1 == '' || cleanedWord2 == '') {
+        result.textContent = "Please enter valid word!";
+        result.style.color = "#e28a8aff";
+    } else if (cleanedWord1 == cleanedWord2) {
+        result.textContent = "Yes, they are anagrams!";
+        result.style.color = "green";
+    } else {
+        result.textContent = "No, they are not anagrams!";
+        result.style.color = "red";
+    }
+}
+
